bea40c98e150b060c6ed4f20b0c90b61af4c81bc
amiit-github <shinde.amit27031991@gmail.com>
2014-01-31 20:23:10 +0530

    beproject

--- functions.js
+++ functions.js
@@ -1,24 +1,27 @@
 var fs = require("fs");
 var OAuth = require('oauth');
 var datum = require('datumbox').factory("247037fb0d191ae468ed13fa7e02ab50");
-
+var FacebookSearch = require('facebook-search');
+var fb = new FacebookSearch('363555700421168', 'ed9ac951c0f286562b521a92ee3686b1');
+var rottenAPI = require("rotten-tomatoes-api")('j6ahmr555pen2ybkb7nsxbb9');
+	
+	//twitter configuration
 	var config = JSON.parse(fs.readFileSync('appconfig/config.json'));
+	var twitter = config.twitter;
 	var oauth = new OAuth.OAuth(
-	      config.requestTokenLink,
-	      config.accessTokenLink,
-	      config.consumerKey,
-	      config.consumerSecret,
+	      twitter.requestTokenLink,
+	      twitter.accessTokenLink,
+	      twitter.consumerKey,
+	      twitter.consumerSecret,
 	      '1.0A',
 	      null,
 	      'HMAC-SHA1'
 	    );
 
-
-
 function getTweetsByREST(url, callback) {
 	oauth.get(url,
-    	config.accessToken,
-    	config.accessSecret,
+    	twitter.accessToken,
+    	twitter.accessSecret,
     	function (e, data, res){
         	if (e) return console.error(e);   
         	var data = JSON.parse(data); 
@@ -27,8 +30,6 @@ function getTweetsByREST(url, callback) {
 }
 
 function getSentiment(doc, callback) {
-
-        		
         		datum.twitterSentimentAnalysis(doc, function(err, result) {
 			    	if ( err ) return console.log(err);
 			    	callback(result);
@@ -36,5 +37,37 @@ function getSentiment(doc, callback) {
 				});      	
 }
 
+function getFacebookPosts(searchQ, callback) {
+	fb.search(searchQ, function(err,res) {
+
+		if ( err ) return console.log(err);
+		callback(res);
+
+		fb.next(function(err, res) {
+		    if ( err ) return console.log(err);
+			callback(res);
+		});
+
+	});
+}
+
+//from rooten tomatoes
+function getMovReviews(movie, callback) {
+	rottenAPI.movieSearch({q:movie, page_limit:1}, function(err,res){
+		if (err) console.log(err);
+
+		//select the first result and find its reviews
+		var movie_id = res.movies[0].id;
+		rottenAPI.movieReviews({id:movie_id, review_type:"top_critic", page_limit:5}, function(err,res){
+		if (err) console.log(err);
+		callback(res);
+	});
+		
+	});
+
+}
+
 module.exports.getTweetsByREST = getTweetsByREST;
 module.exports.getSentiment = getSentiment;
+module.exports.getFacebookPosts = getFacebookPosts;
+module.exports.getMovReviews = getMovReviews;
--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 
 var f = require("./functions.js");
+//var util = require('util');
 
+
+//search tweets on twitter
 f.getTweetsByREST('https://api.twitter.com/1.1/search/tweets.json?q=ronaldo&lang=en', function(data) {
 
   data.statuses.forEach(function(tweet) {
@@ -10,3 +13,26 @@ f.getTweetsByREST('https://api.twitter.com/1.1/search/tweets.json?q=ronaldo&lang
         });
     });
 });
+
+
+/*
+//search facebook posts
+f.getFacebookPosts({type:'post', q: 'beer'}, function(result) {
+	for (item in result) {
+		console.log(result[item].from.name+" : "+result[item].message);
+	}
+	 
+});
+
+
+//Search the movie, 
+f.getMovReviews("Her", function(result) {
+
+		var reviews = result.reviews;
+		for (item in reviews) {
+			console.log(reviews[item].critic+" : "+reviews[item].quote);
+		}
+
+});*/
+
+
